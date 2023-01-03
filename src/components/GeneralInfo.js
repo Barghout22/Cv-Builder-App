b3f3a7298f7000309b47e40b0100c55e81661366
Mahmoud <barghoutmahmoud2@gmail.com>
2023-01-03 19:11:20 +0200

    completed the basic coding for the general info section. the section either displays a form or the group of inputed info. In case of the group if inputed info,there is an edit button that allows us to go back to the form with the inputed info as our initial values. If we don't make any edits after pressing the edit button the initial values are preserved and displayed again.

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -3,35 +3,188 @@ import React, { Component } from "react";
 class GeneralInfo extends Component {
   constructor() {
     super();
+    this.state = {
+      recordedInfo: {
+        name: " ",
+        email: " ",
+        phoneNo: " ",
+        address: " ",
+        about: " ",
+      },
+      editedInfo: {
+        name: " ",
+        email: " ",
+        phoneNo: " ",
+        address: " ",
+        about: " ",
+      },
+      editStatus: true,
+    };
+    this.handleName = this.handleName.bind(this);
+    this.handleEmail = this.handleEmail.bind(this);
+    this.handlePhoneNo = this.handlePhoneNo.bind(this);
+    this.handleAdress = this.handleAdress.bind(this);
+    this.handleAbout = this.handleAbout.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.activateEditStatus = this.activateEditStatus.bind(this);
   }
 
+  activateEditStatus() {
+    this.setState({
+      editStatus: true,
+    });
+  }
+  handleName(event) {
+    this.setState({
+      editedInfo: {
+        email: this.state.editedInfo.email,
+        phoneNo: this.state.editedInfo.phoneNo,
+        address: this.state.editedInfo.address,
+        about: this.state.editedInfo.about,
+        name: event.target.value,
+      },
+    });
+  }
+  handleEmail(event) {
+    this.setState({
+      editedInfo: {
+        name: this.state.editedInfo.name,
+        phoneNo: this.state.editedInfo.phoneNo,
+        address: this.state.editedInfo.address,
+        about: this.state.editedInfo.about,
+        email: event.target.value,
+      },
+    });
+  }
+  handlePhoneNo(event) {
+    this.setState({
+      editedInfo: {
+        name: this.state.editedInfo.name,
+        email: this.state.editedInfo.email,
+        address: this.state.editedInfo.address,
+        about: this.state.editedInfo.about,
+        phoneNo: event.target.value,
+      },
+    });
+  }
+  handleAdress(event) {
+    this.setState({
+      editedInfo: {
+        name: this.state.editedInfo.name,
+        email: this.state.editedInfo.email,
+        phoneNo: this.state.editedInfo.phoneNo,
+        about: this.state.editedInfo.about,
+        address: event.target.value,
+      },
+    });
+  }
+  handleAbout(event) {
+    this.setState({
+      editedInfo: {
+        name: this.state.editedInfo.name,
+        email: this.state.editedInfo.email,
+        phoneNo: this.state.editedInfo.phoneNo,
+        address: this.state.editedInfo.address,
+        about: event.target.value,
+      },
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      recordedInfo: {
+        name:
+          this.state.editedInfo.name === " "
+            ? this.state.recordedInfo.name
+            : this.state.editedInfo.name,
+        email:
+          this.state.editedInfo.email === " "
+            ? this.state.recordedInfo.email
+            : this.state.editedInfo.email,
+        phoneNo:
+          this.state.editedInfo.phoneNo === " "
+            ? this.state.recordedInfo.phoneNo
+            : this.state.editedInfo.phoneNo,
+        address:
+          this.state.editedInfo.address === " "
+            ? this.state.recordedInfo.address
+            : this.state.editedInfo.address,
+        about:
+          this.state.editedInfo.about === " "
+            ? this.state.recordedInfo.about
+            : this.state.editedInfo.about,
+      },
+      editedInfo: {
+        name: " ",
+        email: " ",
+        phoneNo: " ",
+        address: " ",
+        about: " ",
+      },
+      editStatus: false,
+    });
+  }
   render() {
     return (
       <div>
-        <div class="generalInfoTitle">general info</div>
-        <form action="">
-          <label forName="name">
-            name
-            <input type="text" forName="name" />
-          </label>
-          <label forName="email">
-            email
-            <input type="mail" forName="email" />
-          </label>
-          <label forName="phoneNumber">
-            phone number
-            <input type="tel" forName="phoneNumber" />
-          </label>
-          <label forName="address">
-            address
-            <input type="text" forName="address" />
-          </label>
-          <label forName="about">
-            about you
-            <input type="textarea" forName="about" />
-          </label>
-          <button type="submit">add my info</button>
-        </form>
+        <div className="generalInfoTitle">General information</div>
+        {this.state.editStatus && (
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              name
+              <input
+                type="text"
+                defaultValue={this.state.recordedInfo.name}
+                onChange={this.handleName}
+              />
+            </label>
+            <label>
+              email
+              <input
+                type="email"
+                defaultValue={this.state.recordedInfo.email}
+                onChange={this.handleEmail}
+              />
+            </label>
+            <label>
+              phone number
+              <input
+                type="tel"
+                defaultValue={this.state.recordedInfo.phoneNo}
+                onChange={this.handlePhoneNo}
+              />
+            </label>
+            <label>
+              address
+              <input
+                type="text"
+                defaultValue={this.state.recordedInfo.address}
+                onChange={this.handleAdress}
+              />
+            </label>
+            <label>
+              about you
+              <textarea
+                rows="5"
+                columns="33"
+                defaultValue={this.state.recordedInfo.about}
+                onChange={this.handleAbout}
+              />
+            </label>
+            <button type="submit">add my info</button>
+          </form>
+        )}
+        {!this.state.editStatus && (
+          <div>
+            <div>name:{this.state.recordedInfo.name} </div>
+            <div> email:{this.state.recordedInfo.email}</div>
+            <div> phone number:{this.state.recordedInfo.phoneNo}</div>
+            <div> address:{this.state.recordedInfo.address}</div>
+            <div> about:{this.state.recordedInfo.about}</div>
+            <button onClick={this.activateEditStatus}>edit</button>
+          </div>
+        )}
       </div>
     );
   }
