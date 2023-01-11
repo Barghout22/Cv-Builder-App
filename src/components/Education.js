fcbbc3ae209b69667afc7e334a53e3a331eba714
Mahmoud <barghoutmahmoud2@gmail.com>
2023-01-12 00:19:15 +0200

    -changed the work and education sections to functional components. -made some adjustments to the app.css file to make the website more responsive

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,131 +1,92 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import uniqid from "uniqid";
 import DisplayUnitComponent from "./DisplayUnitComponent";
 
-class Education extends Component {
-  constructor() {
-    super();
-    this.state = {
-      educationFields: [],
-      newEducationSection: {
-        studyTitle: " ",
-        schoolName: " ",
-        startDate: " ",
-        endDate: " ",
-        id: uniqid(),
-      },
-      displayNames: {
-        title: "Study title",
-        schoolName: "school name",
-        startDate: "start date",
-        endDate: "end date",
-      },
-      additionStatus: false,
-    };
-    this.toggleAddition = this.toggleAddition.bind(this);
-    this.handleStudyNameChange = this.handleStudyNameChange.bind(this);
-    this.handleSchoolNameChange = this.handleSchoolNameChange.bind(this);
-    this.handleStartChange = this.handleStartChange.bind(this);
-    this.handleEndChange = this.handleEndChange.bind(this);
-    this.addSection = this.addSection.bind(this);
-    this.deleteSection = this.deleteSection.bind(this);
-  }
-  toggleAddition() {
-    this.setState({
-      additionStatus: true,
-    });
-  }
+const Education = () => {
+  const [educationFields, setEducationFields] = useState([]);
 
-  handleStudyNameChange(event) {
-    this.setState({
-      newEducationSection: {
-        studyTitle: event.target.value,
-        schoolName: this.state.newEducationSection.schoolName,
-        startDate: this.state.newEducationSection.startDate,
-        endDate: this.state.newEducationSection.startDate,
-        id: this.state.newEducationSection.id,
-      },
-    });
-  }
-  handleSchoolNameChange(event) {
-    this.setState({
-      newEducationSection: {
-        studyTitle: this.state.newEducationSection.studyTitle,
-        schoolName: event.target.value,
-        startDate: this.state.newEducationSection.startDate,
-        endDate: this.state.newEducationSection.endDAte,
-        id: this.state.newEducationSection.id,
-      },
-    });
-  }
-  handleStartChange(event) {
-    this.setState({
-      newEducationSection: {
-        studyTitle: this.state.newEducationSection.studyTitle,
-        schoolName: this.state.newEducationSection.schoolName,
-        startDate: event.target.value,
-        endDate: this.state.newEducationSection.endDAte,
-        id: this.state.newEducationSection.id,
-      },
-    });
-  }
-  handleEndChange(event) {
-    this.setState({
-      newEducationSection: {
-        studyTitle: this.state.newEducationSection.studyTitle,
-        schoolName: this.state.newEducationSection.schoolName,
-        startDate: this.state.newEducationSection.startDate,
-        endDate: event.target.value,
-        id: this.state.newEducationSection.id,
-      },
-    });
-  }
+  const [newSchooolSectionStudyTitle, setNewSchoolSectionStudyTitle] =
+    useState("");
+  const [newSchoolSectionSchoolName, setnewSchoolSectionSchoolName] =
+    useState("");
+  const [newSchoolSectionStartDate, setNewSchoolSectionStartDate] =
+    useState("");
+  const [newSchoolSectionEndDate, setNewSchoolSectionEndDate] = useState("");
+  const [newSchoolSectionId, setNewSchoolSectionId] = useState(uniqid());
 
-  addSection() {
-    this.setState({
-      educationFields: this.state.educationFields.concat(
-        this.state.newEducationSection
-      ),
-      newEducationSection: {
-        studyTitle: " ",
-        schoolName: " ",
-        startDate: " ",
-        endDate: " ",
-        id: uniqid(),
-      },
-      additionStatus: false,
-    });
-  }
-  deleteSection(id) {
-    this.setState({
-      educationFields: this.state.educationFields.filter((item) => {
-        return item.id !== id;
-      }),
-    });
-  }
+  const [additionStatus, setAdditionStatus] = useState(false);
+  const displayNames = {
+    title: "Study title",
+    schoolName: "school name",
+    startDate: "start date",
+    endDate: "end date",
+  };
 
-  render() {
-    return (
-      <div>
-        <div className="sectionHead">Education Info</div>
-        <DisplayUnitComponent
-          educationFields={this.state.educationFields}
-          displayNames={this.state.displayNames}
-          additionStatus={this.state.additionStatus}
-          toggleAddition={this.toggleAddition}
-          handleStudyNameChange={this.handleStudyNameChange}
-          handleSchoolNameChange={this.handleSchoolNameChange}
-          handleStartChange={this.handleStartChange}
-          handleEndChange={this.handleEndChange}
-          addSection={this.addSection}
-          deleteSection={this.deleteSection}
-        />
-        {!this.state.additionStatus && (
-          <button onClick={this.toggleAddition}>Add Experience</button>
-        )}
-      </div>
+  const toggleAddition = () => {
+    setAdditionStatus(true);
+  };
+
+  const handleStudyNameChange = (event) => {
+    setNewSchoolSectionStudyTitle(event.target.value);
+  };
+  const handleSchoolNameChange = (event) => {
+    setnewSchoolSectionSchoolName(event.target.value);
+  };
+
+  const handleStartChange = (event) => {
+    setNewSchoolSectionStartDate(event.target.value);
+  };
+
+  const handleEndChange = (event) => {
+    setNewSchoolSectionEndDate(event.target.value);
+  };
+
+  const addSection = () => {
+    setEducationFields(
+      educationFields.concat({
+        studyTitle: newSchooolSectionStudyTitle,
+        schoolName: newSchoolSectionSchoolName,
+        startDate: newSchoolSectionStartDate,
+        endDAte: newSchoolSectionEndDate,
+        id: newSchoolSectionId,
+      })
     );
-  }
-}
+    setNewSchoolSectionStudyTitle("");
+    setnewSchoolSectionSchoolName("");
+    setNewSchoolSectionStartDate("");
+    setNewSchoolSectionEndDate("");
+    setNewSchoolSectionId(uniqid());
+    setAdditionStatus(false);
+  };
+
+  const deleteSection = (id) => {
+    setEducationFields(
+      educationFields.filter((item) => {
+        return item.id !== id;
+      })
+    );
+  };
+
+  return (
+    <div>
+      <div className="sectionHead">Education information</div>
+      <DisplayUnitComponent
+        educationFields={educationFields}
+        displayNames={displayNames}
+        additionStatus={additionStatus}
+        toggleAddition={toggleAddition}
+        handleStudyNameChange={handleStudyNameChange}
+        handleSchoolNameChange={handleSchoolNameChange}
+        handleStartChange={handleStartChange}
+        handleEndChange={handleEndChange}
+        addSection={addSection}
+        deleteSection={deleteSection}
+      />
+      {!additionStatus && (
+        <button onClick={toggleAddition}>Add Experience</button>
+      )}
+    </div>
+  );
+};
 
 export default Education;
--- src/components/Workinfo.js
+++ src/components/Workinfo.js
@@ -1,16 +1,17 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import uniqid from "uniqid";
 import DisplayUnitComponent from "./DisplayUnitComponent";
 
 const WorkInfo = () => {
   const [WorkFields, setWorkFields] = useState([]);
-  const [newWorkSection, setNewWorkSection] = useState({
-    workTitle: " ",
-    companyName: " ",
-    startDate: " ",
-    endDate: " ",
-    id: uniqid(),
-  });
+
+  const [newWorkSectionWorkTitle, setNewWorkSectionWorkTitle] = useState("");
+  const [newWorkSectionCompanyName, setNewWorkSectionCompanyName] =
+    useState("");
+  const [newWorkSectionStartDate, setNewWorkSectionStartDate] = useState("");
+  const [newWorkSectionEndDate, setNewWorkSectionEndDate] = useState("");
+  const [newWorkSectionId, setNewWorkSectionId] = useState(uniqid());
+
   const [additionStatus, setAdditionStatus] = useState(false);
   const displayNames = {
     title: "work title",
@@ -24,29 +25,35 @@ const WorkInfo = () => {
   };
 
   const handleStudyNameChange = (event) => {
-    setNewWorkSection((newWorkSection.workTitle = event.target.value));
+    setNewWorkSectionWorkTitle(event.target.value);
   };
   const handleSchoolNameChange = (event) => {
-    setNewWorkSection((newWorkSection.companyName = event.target.value));
+    setNewWorkSectionCompanyName(event.target.value);
   };
 
   const handleStartChange = (event) => {
-    setNewWorkSection((newWorkSection.startDate = event.target.value));
+    setNewWorkSectionStartDate(event.target.value);
   };
 
   const handleEndChange = (event) => {
-    setNewWorkSection((newWorkSection.endDate = event.target.value));
+    setNewWorkSectionEndDate(event.target.value);
   };
 
   const addSection = () => {
-    setWorkFields(WorkFields.concat(newWorkSection));
-    setNewWorkSection({
-      workTitle: " ",
-      companyName: " ",
-      startDate: " ",
-      endDAte: " ",
-      id: uniqid(),
-    });
+    setWorkFields(
+      WorkFields.concat({
+        workTitle: newWorkSectionWorkTitle,
+        companyName: newWorkSectionCompanyName,
+        startDate: newWorkSectionStartDate,
+        endDAte: newWorkSectionEndDate,
+        id: newWorkSectionId,
+      })
+    );
+    setNewWorkSectionWorkTitle("");
+    setNewWorkSectionCompanyName("");
+    setNewWorkSectionStartDate("");
+    setNewWorkSectionEndDate("");
+    setNewWorkSectionId(uniqid());
     setAdditionStatus(false);
   };
 
@@ -79,130 +86,5 @@ const WorkInfo = () => {
     </div>
   );
 };
-
-// class WorkInfo extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       WorkFields: [],
-//       newWorkSection: {
-//         workTitle: " ",
-//         companyName: " ",
-//         startDate: " ",
-//         endDAte: " ",
-//         id: uniqid(),
-//       },
-//       displayNames: {
-//         title: "work title",
-//         schoolName: "company name",
-//         startDate: "start date",
-//         endDate: "end date",
-//       },
-//       additionStatus: false,
-//     };
-//     this.toggleAddition = this.toggleAddition.bind(this);
-//     this.handleStudyNameChange = this.handleStudyNameChange.bind(this);
-//     this.handleSchoolNameChange = this.handleSchoolNameChange.bind(this);
-//     this.handleStartChange = this.handleStartChange.bind(this);
-//     this.handleEndChange = this.handleEndChange.bind(this);
-//     this.addSection = this.addSection.bind(this);
-//     this.deleteSection = this.deleteSection.bind(this);
-//   }
-
-//   toggleAddition() {
-//     this.setState({
-//       additionStatus: true,
-//     });
-//   }
-
-//   handleStudyNameChange(event) {
-//     this.setState({
-//       newWorkSection: {
-//         workTitle: event.target.value,
-//         companyName: this.state.newWorkSection.companyName,
-//         startDate: this.state.newWorkSection.startDate,
-//         endDate: this.state.newWorkSection.startDate,
-//         id: this.state.newWorkSection.id,
-//       },
-//     });
-//   }
-//   handleSchoolNameChange(event) {
-//     this.setState({
-//       newWorkSection: {
-//         workTitle: this.state.newWorkSection.workTitle,
-//         companyName: event.target.value,
-//         startDate: this.state.newWorkSection.startDate,
-//         endDate: this.state.newWorkSection.endDAte,
-//         id: this.state.newWorkSection.id,
-//       },
-//     });
-//   }
-//   handleStartChange(event) {
-//     this.setState({
-//       newWorkSection: {
-//         workTitle: this.state.newWorkSection.workTitle,
-//         companyName: this.state.newWorkSection.companyName,
-//         startDate: event.target.value,
-//         endDate: this.state.newWorkSection.endDAte,
-//         id: this.state.newWorkSection.id,
-//       },
-//     });
-//   }
-//   handleEndChange(event) {
-//     this.setState({
-//       newWorkSection: {
-//         workTitle: this.state.newWorkSection.workTitle,
-//         companyName: this.state.newWorkSection.companyName,
-//         startDate: this.state.newWorkSection.startDate,
-//         endDate: event.target.value,
-//         id: this.state.newWorkSection.id,
-//       },
-//     });
-//   }
-
-//   addSection() {
-//     this.setState({
-//       WorkFields: this.state.WorkFields.concat(this.state.newWorkSection),
-//       newWorkSection: {
-//         workTitle: " ",
-//         companyName: " ",
-//         startDate: " ",
-//         endDAte: " ",
-//         id: uniqid(),
-//       },
-//       additionStatus: false,
-//     });
-//   }
-//   deleteSection(id) {
-//     this.setState({
-//       WorkFields: this.state.WorkFields.filter((item) => {
-//         return item.id !== id;
-//       }),
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <div className="sectionHead">Work information</div>
-//         <DisplayUnitComponent
-//           educationFields={this.state.WorkFields}
-//           displayNames={this.state.displayNames}
-//           additionStatus={this.state.additionStatus}
-//           toggleAddition={this.toggleAddition}
-//           handleStudyNameChange={this.handleStudyNameChange}
-//           handleSchoolNameChange={this.handleSchoolNameChange}
-//           handleStartChange={this.handleStartChange}
-//           handleEndChange={this.handleEndChange}
-//           addSection={this.addSection}
-//           deleteSection={this.deleteSection}
-//         />
-//         {!this.state.additionStatus && (
-//           <button onClick={this.toggleAddition}>Add Experience</button>
-//         )}
-//       </div>
-//     );
-//   }
-// }
 
 export default WorkInfo;
